61345c047b1150235d1d229260804f8b3e76da51
Lucas <nayevskau@gmail.com>
2022-11-07 21:46:54 -0300

    Update

--- Primeiro Projeto com NodeJS/src/index.js
+++ Primeiro Projeto com NodeJS/src/index.js
@@ -1,42 +1,48 @@
 // importa o express:
-const { response } = require('express');
 const express = require('express');
-
+const {v4:uuidv4} = require("uuid");
 const app = express();
 
-//define a porte a ser utilizada. No caso a direcao eh localhost:3333 || 127.0.0.1:3333
 app.listen(3333);
+app.use(express.json());
 
-//Necessario para informar ao express o tipo de parametro que sera recebido dentro do body.
-app.use(express.json())
+const customers = []
+
+/** Account Interface:
+ * - name - string
+ * - cpf - string
+ * - id - uuid
+ * - statement - []
+ */
 
 
-app.get("/courses", (request,response) => {
-  const query = request.query
-  console.log(query)
-  return response.json(["curso 1","curso 2","curso 3"]);
+app.post("/accounts", (request,response) => {
+  const {cpf,name} = request.body;
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({error: "this customer already exists"})
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id:uuidv4(),
+    statement:[]
+  });
+  return response.status(201).send()
 });
 
-app.post("/courses", (request,response) => {
-  const body = request.body;
-  console.log(body)
-  return response.json(["curso 1","curso 2","curso 3","curso 4"]);
+app.get("/statement/:cpf", (request, response) => {
+  const {cpf} = request.params;
+
+  const customer = customers.find((customer) =>
+    customer.cpf === cpf
+  );
+
+  if(!customer){
+    return response.status(400).json({error:"Customer not found"})
+  };
+
+  return response.json(customer.statement);
 });
-
-// :id representa o parameto 
-app.put("/courses/:id", (request,response) => {
-  const param = request.params;
-  console.log(param)
-  return response.json(["curso 6","curso 2","curso 3","curso 4"]);
-});
-
-app.patch("/courses/:id", (request,response) => {
-  return response.json(["curso 6","curso 7","curso 3","curso 4"]);
-});
-
-app.delete("/courses/:id", (request,response) => {
-  return response.json(["curso 6","curso 2","curso 4"]);
-});
-
-
-//Para executar esse setup: node src/index.js
