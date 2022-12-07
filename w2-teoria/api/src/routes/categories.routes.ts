5ab35ba0f9c239411077ca495b81d41194e914d9
Lucas <nayevskau@gmail.com>
2022-12-07 19:09:23 -0300

    update

--- w2-teoria/api/src/routes/categories.routes.ts
+++ w2-teoria/api/src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/car/useCases/createCategory";
 import { listCategoriesController } from "../modules/car/useCases/listCategories"
-
+import { importCategoryController } from "../modules/car/useCases/importCategory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp"
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response)
@@ -12,6 +17,11 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+})
+
+// .single usado para receber apenas um arquivo por vez, multer funciona como um middleware.
+categoriesRoutes.post("/file", upload.single("file"), (request,response) => {
+  return importCategoryController.handle(request,response);
 })
 
 export { categoriesRoutes }
