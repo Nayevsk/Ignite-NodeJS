635fbfb4cb353eb5282a7b9ceeb6102f029efeee
Lucas <nayevskau@gmail.com>
2022-12-08 15:33:51 -0300

    settingup swagger

--- w2-teoria/api/src/server.ts
+++ w2-teoria/api/src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+// Se essa importacao n funcionar ir ate tsconfig.json e "resolveJsonModule": true,  
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
