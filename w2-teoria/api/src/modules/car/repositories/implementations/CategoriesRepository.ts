227efa19c5679ece8647a518f79c800f9d907f89
Lucas <nayevskau@gmail.com>
2022-12-08 14:58:18 -0300

    adding import files to mock database

--- w2-teoria/api/src/modules/car/repositories/implementations/CategoriesRepository.ts
+++ w2-teoria/api/src/modules/car/repositories/implementations/CategoriesRepository.ts
@@ -13,7 +13,7 @@ class CategoriesRepository implements ICategoriesRepository {
   }
 
   public static getInstance(): CategoriesRepository {
-
+// Estrutura Singleton
     if (!CategoriesRepository.INSTANCE) {
       CategoriesRepository.INSTANCE = new CategoriesRepository();
     }
